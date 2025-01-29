ae4769b6b4a8c879695c51bd45c6ff08e3564dae
AvanishVadke <avanishvadke001@gmail.com>
2025-01-29 18:48:41 +0530

    Refactor Firebase configuration to use environment variables for improved security

--- src/firebase.js
+++ src/firebase.js
@@ -7,12 +7,12 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDh5CilVgSq9MVRKCozfCIe2_q_aQ82gl0",
-  authDomain: "email-and-auth-app.firebaseapp.com",
-  projectId: "email-and-auth-app",
-  storageBucket: "email-and-auth-app.firebasestorage.app",
-  messagingSenderId: "326755469157",
-  appId: "1:326755469157:web:c9b0b1e17d415727e3438c"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
